258964fcd1c5a33c25c0495b046d0b97be549ab4
EKracht <ethan1kracht@gmail.com>
2015-11-09 23:41:43 -0800

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$(document).ready(init);
+
+function init(){
+  $('button').on('click', sendReq);
+}
+
+function sendReq(){
+  var input = $('.input').val();
+
+  $.ajax({
+    type: "POST",
+    url: "/",
+    data: {string: input}
+  })
+  .done(function(data){
+    $('.output').empty();
+    var parsedData = $(data);
+    $('.output').append(parsedData);
+  })
+  .fail(function(error){
+  })
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var marked = require('marked');
+
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.post('/', function(req, res){
+  var markNowHTML = convertMarkdown(req.body.string);
+  res.send(markNowHTML);
+});
+
+function convertMarkdown(str){
+  var markStr = marked(str);
+  return markStr;
+}
+
+app.listen(3000);
